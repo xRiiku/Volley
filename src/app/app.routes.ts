f4bafa7ac4c8059f24d28cee04126a3df4231c80
Daniel Ramon Campos <dramon@clicktotravel.es>
2025-11-09 15:47:59 +0100

    .

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+  { path: '', loadComponent: () => import('../pages/home/home.component').then(m => m.HomeComponent) },
+
+];
---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  standalone: true
+})
+export class HomeComponent {
+
+}
